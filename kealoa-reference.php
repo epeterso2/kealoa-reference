7d18a08b2d431314ac866ba4fc31c28b009f023e
Eric <eric@puzzlehead.org>
2026-02-14 15:40:52 -0500

    Bumping version

--- kealoa-reference.php
+++ kealoa-reference.php
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Plugin constants
-define('KEALOA_VERSION', '1.0.72');
+define('KEALOA_VERSION', '1.0.73');
 define('KEALOA_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('KEALOA_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('KEALOA_PLUGIN_BASENAME', plugin_basename(__FILE__));
