20c317adbdc642ec8fc33477915113854628b274
Eric Peterson <eric@puzzlehead.org>
2026-02-26 15:12:09 -0500

    Fixing release script

--- relkealoa.sh
+++ relkealoa.sh
@@ -5,5 +5,15 @@
 # @copyright 2026 Eric Peterson (eric@puzzlehead.org)
 # @license   CC BY-NC-SA 4.0 <https://creativecommons.org/licenses/by-nc-sa/4.0/>
 #
-rm kealoa-reference.zip && \
-zip -r kealoa-reference kealoa-reference
+
+PLUGIN_ZIP=kealoa-reference.zip
+
+rm -f ${PLUGIN_ZIP}
+if [ -f ${PLUGIN_ZIP} ]
+then
+	echo Plugin file still exists: ${PLUGIN_ZIP}
+	exit 1
+else
+	zip -r kealoa-reference kealoa-reference
+fi
+
